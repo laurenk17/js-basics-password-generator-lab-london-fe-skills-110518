3ce055580b953adec6e2cc1f62c311415b3f25a6
lauren kernan <lauren.kernan17@gmail.com>
2019-01-14 18:28:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ window.addEventListener("load", () => {
   //start your code here
   function passwordGenerator(len){
     let length = getElementById("length");
-    let charset1 = ["a","b","c","d","e","f","g","h","i","j","k","l","m" "n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    let charset1 = ["a","b","c","d","e","f","g","h","i","j","k","l","m", "n","o","p","q","r","s","t","u","v","w","x","y","z"];
     let charset2 = ["A","B","C","D","E","F","G","H","I","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
     let charset3 = ["0","1","2","3","4","5","6","7","8","9"];
     let charset4 = ["!#$%&()*+,-./:;<=>?@[\]^_{|}~"];
